46fd1b29b4fa2ab2756c4b784567bce554c12a7d
User <User@gmail.com>
2023-02-02 23:48:11 -0300

    add

---
+++ rice.mk
@@ -0,0 +1,19 @@
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigSoong.mk)
+$(call inherit-product, vendor/lineage/config/BoardConfigLineage.mk)
+$(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
+-include vendor/lineage/build/core/config.mk
+
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+SELINUX_IGNORE_NEVERALLOWS := true
+
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_NO_KERNEL_IMAGE := true
+
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USE_PIXEL_FINGERPRINT := true
+SUSHI_BOOTANIMATION := 1080
+RICE_PACKAGE_TYPE := VANILLA
+RICE_MAINTAINER := TheAtt1la
